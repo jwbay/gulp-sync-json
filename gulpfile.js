c258c53ea9711c89dede4ab41a24c067fa07be3d
Justin Bay <justin.bay@outlook.com>
2015-05-23 23:07:13 -0400

    consume stream

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
-var gulp = require("gulp");
+var gulp = require('gulp');
+var through = require('through2');
+
+function syncDirectory() {
+	var stream = through.obj(function(file, enc, cb) {
+		console.log(file.path);		
+		this.push(file);
+	});
+	
+	return stream;
+}
 
 gulp.task("default", function () {
-	console.log('hello world');	
+	gulp.src("./test/*.json")
+		.pipe(syncDirectory())
+		.pipe(gulp.dest("./test/"));
 });
