c3ba1cea6bfc85042f286c29bed5e7a513e1f4b0
Justin Bay <justin.bay@outlook.com>
2015-05-25 00:24:16 -0400

    add reporting/logging

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var through = require('through2');
 var PluginError = gutil.PluginError;
+var log = gutil.log;
 
 'use strict';
 
@@ -24,15 +25,26 @@ function syncDirectory(primaryFile) {
 	function processFiles(done) {
 		var _this = this;
 		var sourceKeys = bufferToObject(source.contents);
+		var resultMap = {};
 
-		//give source file the same re-serialization for whitespace as target files
 		source.contents = objectToBuffer(sourceKeys);
 		_this.push(source);
 
 		targets.forEach(function (target) {
 			var targetKeys = bufferToObject(target.contents);
-			sync(sourceKeys, targetKeys);
+			resultMap[target.path] = sync(sourceKeys, targetKeys);
 			target.contents = objectToBuffer(targetKeys);
+		});
+		
+		targets.forEach(function(target) {
+			var fileName = target.path.replace(target.cwd, '');
+			var result = resultMap[target.path];
+			if (result.pushed.length) {
+				log('Pushed to', gutil.colors.cyan(fileName) + ':', getResultString(result.pushed));
+			}
+			if (result.removed.length) {
+				log('Removed from', gutil.colors.cyan(fileName) + ':', getResultString(result.removed));
+			}
 			_this.push(target);
 		});
 
@@ -40,13 +52,19 @@ function syncDirectory(primaryFile) {
 	}
 
 	function sync(source, target) {
+		var pushedKeys = [];
+		var removedKeys = [];
+		
 		Object.keys(source).forEach(function (key) {
 			if (!target.hasOwnProperty(key)) {
 				if (typeof source[key] === 'string') {
+					pushedKeys.push(key);
 					target[key] = source[key];
 				} else {
 					target[key] = {};
-					sync(source[key], target[key]);
+					var result = sync(source[key], target[key]);
+					[].push.apply(pushedKeys, result.pushed);
+					[].push.apply(removedKeys, result.removed);
 				}
 			} else {
 				if (typeof source[key] !== typeof target[key]) {
@@ -54,11 +72,18 @@ function syncDirectory(primaryFile) {
 				}
 			}
 		});
+		
 		Object.keys(target).forEach(function (key) {
 			if (!source.hasOwnProperty(key)) {
 				delete target[key];
+				removedKeys.push(key);
 			}
 		});
+		
+		return {
+			pushed: pushedKeys,
+			removed: removedKeys
+		};
 	}
 
 	function bufferToObject(buffer) {
@@ -69,6 +94,24 @@ function syncDirectory(primaryFile) {
 	function objectToBuffer(object) {
 		var contents = JSON.stringify(object, null, 4);
 		return new Buffer(contents);
+	}
+	
+	function stringifyKeyList(array) {
+		return array.map(function(key) {
+			return gutil.colors.cyan(key);
+		}).join(", ");
+	}
+	
+	function getResultString(keysArray) {
+		if (keysArray.length <= 3) {
+			return stringifyKeyList(keysArray);
+		}
+		return [
+			stringifyKeyList(keysArray.slice(0, 3)),
+			' and ',
+			gutil.colors.magenta(keysArray.length - 3),
+			' more'
+		].join('');
 	}
 
 	return through.obj(addFiles, processFiles);
