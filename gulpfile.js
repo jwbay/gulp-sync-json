04f90a5f5a8cb81a61d6cd5e3688945cfcaf23a7
Justin Bay <justin.bay@outlook.com>
2015-05-25 22:56:40 -0400

    remove testdata

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var syncJSON = require('./');
-
-gulp.task('default', function () {
-	return gulp.src('./**/*.json')
-		.pipe(syncJSON('en.json'))
-		.pipe(gulp.dest('./'));
-});
